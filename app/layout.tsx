12df41d67c544983a4d1397f7b8110fca4ac3713
Riley Kacmarynski <riley.kacmarynski@gmail.com>
2023-04-16 20:08:55 -0500

    move to app dir, start on auth

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import { Inter as FontSans } from "next/font/google"
+
+import "@/styles/globals.css"
+import { siteConfig } from "@/config/site"
+import { absoluteUrl, cn } from "@/lib/utils"
+import { TailwindIndicator } from "@/components/tailwind-indicator"
+import { Toaster } from "@/components/ui/toaster"
+import { ThemeProvider } from "next-themes"
+import { SiteHeader } from "@/components/site-header"
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export const metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+}
+
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        'text-slate-950 dark:bg-slate-950 bg-white font-sans antialiased dark:text-slate-50',
+        fontSans.variable
+      )}>
+      <body className="min-h">
+        {/* <ThemeProvider attribute="class" defaultTheme="system" enableSystem> */}
+        <SiteHeader />
+        <main>{children}</main>
+        <Toaster />
+        <TailwindIndicator />
+        {/* </ThemeProvider> */}
+      </body>
+    </html >
+  )
+}
